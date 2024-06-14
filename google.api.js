3c784bbde922102f509d025156a09ff34c46d1c1
viktorhrimli <viktorhrimli101@gmail.com>
2024-06-14 10:27:00 +0300

    commit bag and not finished code

--- google.api.js
+++ google.api.js
@@ -1,5 +1,30 @@
 const base = "https://api-m.sandbox.paypal.com";
 
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET } = process.env;
+
+export const generateAccessToken = async () => {
+  try {
+    if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+      throw new Error("MISSING_API_CREDENTIALS");
+    }
+    const auth = Buffer.from(
+      PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET
+    ).toString("base64");
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "POST",
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
+
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error("Failed to generate Access Token:", error);
+  }
+};
+
 export async function createOrder() {
   const purchaseAmount = "100.00";
   const accessToken = await generateAccessToken();
@@ -15,7 +40,7 @@ export async function createOrder() {
       purchase_units: [
         {
           amount: {
-            currency_code: "USD",
+            currency_code: "EUR",
             value: purchaseAmount,
           },
         },
--- server.js
+++ server.js
@@ -6,17 +6,14 @@ import path from "path";
 
 import * as PayPal from "./google.api.js";
 
+import { LiqPay } from "./liqpay.js";
 const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8888 } = process.env;
 const base = "https://api-m.sandbox.paypal.com";
 const app = express();
 
 app.use(express.static("client/dist"));
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -122,7 +119,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve("./client/dist/index.html"));
 });
 
-// return client token for hosted-fields component
+// app.post("/api/googleorders", async (req, res) => {
+//   const order = await PayPal.createOrder();
+//   res.json(order);
+// });
+// /* Capture Order route Handler */
+// app.post("/api/googleorders/:orderID/capture", async (req, res) => {
+//   const { orderID } = req.params;
+//   console.log(orderID, "GOOGLEOREDERS");
+//   const captureData = await PayPal.capturePayment(orderID);
+//   res.json(captureData);
+// });
+
 app.post("/api/token", async (req, res) => {
   try {
     const { jsonResponse, httpStatusCode } = await generateClientToken();
@@ -135,7 +143,6 @@ app.post("/api/token", async (req, res) => {
 
 app.post("/api/orders", async (req, res) => {
   try {
-    // use the cart information passed from the front-end to calculate the order amount detals
     const { cart } = req.body;
     const { jsonResponse, httpStatusCode } = await createOrder(cart);
     res.status(httpStatusCode).json(jsonResponse);
@@ -154,6 +161,48 @@ app.post("/api/orders/:orderID/capture", async (req, res) => {
     console.error("Failed to create order:", error);
     res.status(500).json({ error: "Failed to capture order." });
   }
+});
+
+app.post("/api/confirmOrder", async (req, res) => {
+  try {
+    const accessToken = await generateAccessToken();
+    console.log(accessToken);
+    const response = await fetch(
+      "https://www.sandbox.paypal.com/graphql?ApproveGooglePayPayment",
+      {
+        method: "POST",
+        body: JSON.stringify(req.body),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    console.log(response, "WTF");
+    res.json(response);
+  } catch (error) {
+    console.error("Ошибка при обработке запроса к PayPal:", error);
+    res.status(500).json({ error: "Ошибка при обработке запроса" });
+  }
+});
+
+app.get("/liqpay/form", async (req, res) => {
+  var liqpaySDK = new LiqPay(
+    process.env.LIQPAY_SUNDBOX_PUBLIC_KEY,
+    process.env.LIQPAY_SUNDBOX_PRIVET_KEY
+  );
+  var html = liqpaySDK.cnb_form({
+    action: "pay",
+    // public_key: process.env.LIQPAY_SUNDBOX_PUBLIC_KEY,
+    amount: "50",
+    currency: "UAH",
+    description: "description text",
+    order_id: "123123",
+    version: "3",
+  });
+
+  res.status(200).json({ form: html });
 });
 
 app.listen(PORT, () => {
